4765e7ab41da1d8c7757d8b69dfe9977b16fe968
yummydum <atsushisumita0421@gmail.com>
2020-12-02 13:23:22 +0900

    fix date format & add logger for query

--- api_server.py
+++ api_server.py
@@ -1,19 +1,24 @@
+import logging
 import sqlite3
+from datetime import datetime
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+logging.basicConfig(level=logging.INFO)
+date_format = '%Y-%m-%d'
 
 
 @app.route('/data')
 def extract_data():
     column = request.args.get('measure')
     q = f"SELECT date,{column} FROM room WHERE room.date BETWEEN ? AND ?"
-    start_date = request.args.get('start_date')
-    end_date = request.args.get('end_date')
+    start_date = datetime.strptime(request.args.get('start_date'), date_format)
+    end_date = datetime.strptime(request.args.get('end_date'), date_format)
     with sqlite3.connect('room') as con:
+        con.set_trace_callback(logging.info)
         c = con.cursor()
         c.execute(q, (start_date, end_date))
         data = c.fetchall()
@@ -24,4 +29,4 @@ def extract_data():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
